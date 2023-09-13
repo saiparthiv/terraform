8fb8584e24b673866ae7276eae8f74cdd0278c55
saiparthiv <saiparthiv95@gmail.com>
2023-09-13 01:51:39 -0500

    Made some changes to files

--- first_script.tf
+++ first_script.tf
@@ -1,15 +1,15 @@
-# Define the provider for AWS
-provider "aws" {
-  region = "us-east-1" # Change this to your desired AWS region
+# Specify the key name to use for SSH access (adjust to your own key pair)
+resource "aws_key_pair" "dove-key" {
+  key_name   = "dovekey"
+  public_key = file("dovekey.pub")
 }
 
 # Define the EC2 instance resource
 resource "aws_instance" "example" {
-  ami           = "ami-053b0d53c279acc90"
-  instance_type = "t3.micro"
+  ami           = var.aws_amis
+  instance_type = var.aws_instance_type
+  key_name      = aws_key_pair.dove-key.key_name
 
-  # Specify the key name to use for SSH access (adjust to your own key pair)
-  key_name = "web-dev-key"
 
   # Security group
   vpc_security_group_ids = ["sg-0623636f219772a1a"]
@@ -19,4 +19,31 @@ resource "aws_instance" "example" {
     Name  = "Launched By Terraform"
     Tools = "Terraform"
   }
+
+  provisioner "file" {
+    source      = "web.sh"
+    destination = "/tmp/web.sh"
+  }
+
+  provisioner "remote-exec" {
+
+    inline = [
+      "chmod +x /tmp/web.sh",
+      "sudo /tmp/web.sh"
+    ]
+  }
+
+  connection {
+    user        = "ubuntu"
+    private_key = file("dovekey")
+    host        = self.public_ip
+  }
+}
+
+output "PublicIP" {
+  value = aws_instance.example.public_ip
+}
+
+output "PrivateIP" {
+  value = aws_instance.example.private_ip
 }
